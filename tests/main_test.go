f0b3dfa5313053b5f380aae86a087f6a15099383
Avyukth <makur.subhrajit@gmail.com>
2023-09-22 16:53:27 +0530

    adding test cases and function

--- tests/main_test.go
+++ tests/main_test.go
@@ -8,9 +8,19 @@ func TestGenerateInsertSQL(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "Insert Operation",
-			oplog:   "",
-			want:    "",
+			name: "Insert Operation",
+			oplog: `{
+  "op" : "i",
+  "ns" : "test.student",
+  "o" : {
+    "_id" : "635b79e231d82a8ab1de863b",
+    "name" : "Selena Miller",
+    "roll_no" : 51,
+    "is_graduated" : false,
+    "date_of_birth" : "2000-01-30"
+  }
+}`,
+			want:    "INSERT INTO test.student (_id, date_of_birth, is_graduated, name, roll_no) VALUES ('635b79e231d82a8ab1de863b', '2000-01-30', false, 'Selena Miller', 51);",
 			wantErr: false,
 		},
 	}
